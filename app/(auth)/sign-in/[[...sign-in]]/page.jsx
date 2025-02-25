b0dc618bc9b68788503d34cfc36c9733c4c32941
MenathNDGD <nadungmenath1@gmail.com>
2025-02-26 01:01:33 +0530

    Add sign-in page with background image and Clerk SignIn component

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,21 @@
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="bg-white flex">
+      <div className="hidden lg:block fixed left-0 top-0 h-full w-1/2">
+        <Image
+          src={"/signin_bg.jpg"}
+          layout="fill"
+          objectFit="cover"
+          alt="SignIn"
+        />
+      </div>
+
+      <div className="fixed right-0 top-0 h-full w-full lg:w-1/2 flex justify-center items-center">
+        <SignIn />
+      </div>
+    </div>
+  );
+}
