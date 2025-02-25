45bc90c72dba6e71a42ad0b5f33bd166c866d1fd
MenathNDGD <nadungmenath1@gmail.com>
2025-02-26 01:01:17 +0530

    Wrap RootLayout in ClerkProvider for authentication context

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Provider from "./Provider";
 
+import { ClerkProvider } from "@clerk/nextjs";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,10 +13,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Provider>{children}</Provider>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Provider>{children}</Provider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
